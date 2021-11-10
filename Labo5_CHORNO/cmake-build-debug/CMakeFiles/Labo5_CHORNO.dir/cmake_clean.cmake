23c3c750e2003c6960c4d4038af49e29dc9a090d
Kossi-lok <71429614+Kossi-lok@users.noreply.github.com>
2021-11-10 12:04:04 +0100

    Add files via upload

---
+++ Labo5_CHORNO/cmake-build-debug/CMakeFiles/Labo5_CHORNO.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Labo5_CHORNO.dir/main.cpp.obj"
+  "Labo5_CHORNO.exe"
+  "Labo5_CHORNO.exe.manifest"
+  "Labo5_CHORNO.pdb"
+  "libLabo5_CHORNO.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Labo5_CHORNO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Labo5_CHORNO/cmake-build-debug/CMakeFiles/Labo5_CHORNO.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Labo5_CHORNO.
+# This may be replaced when dependencies are built.
